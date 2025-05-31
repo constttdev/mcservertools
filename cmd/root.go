c7a69301d24fb61d253af86feabfae906b6c0146
constt <175602385+constttdev@users.noreply.github.com>
2025-05-31 20:29:15 +0200

    feat: moved main command and switched it with cmds command

---
+++ cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "cmds",
+	Short: "List all available commands",
+	Long:  `Using this command you can see all available commands`,
+	Run: func(cmd *cobra.Command, args []string) {
+		hiBlue := color.New(color.FgHiBlue).SprintFunc()
+
+		fmt.Println(hiBlue("Available commands: \n\nrconconnect <address> <password>\nserverinfo <address>\nportscan <address>\n\n"))
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
