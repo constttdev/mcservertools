1a2a2dda071bf99eca07ea9f9b9b94fe6304421a
constt <175602385+constttdev@users.noreply.github.com>
2025-06-01 00:22:52 +0200

    feat: geolocation command

---
+++ cmd/geolocation.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+type GeoInfo struct {
+	Country     string  `json:"country"`
+	CountryCode string  `json:"countryCode"`
+	Region      string  `json:"region"`
+	City        string  `json:"city"`
+	ISP         string  `json:"isp"`
+	Org         string  `json:"org"`
+	AS          string  `json:"as"`
+	Query       string  `json:"query"`
+	Lat         float64 `json:"lat"`
+	Lon         float64 `json:"lon"`
+}
+
+func init() {
+	rootCmd.AddCommand(geolocationCmd)
+	geolocationCmd.Flags().StringVarP(&ip, "ip", "", "", "IP address to locate")
+	geolocationCmd.MarkFlagRequired("ip")
+}
+
+var ip string
+
+var geolocationCmd = &cobra.Command{
+	Use:   "locateip",
+	Short: "See the approximate location of an ip",
+	Long:  `See the approximate location of an ip via gelocation`,
+	Run: func(cmd *cobra.Command, args []string) {
+		green := color.New(color.FgGreen).SprintFunc()
+		yellow := color.New(color.FgYellow).SprintFunc()
+
+		fmt.Printf(green("Getting the approximate location of: %s\n\n"), ip)
+
+		resp, err := http.Get("http://ip-api.com/json/" + ip)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		var geo GeoInfo
+		if err := json.NewDecoder(resp.Body).Decode(&geo); err != nil {
+			panic(err)
+		}
+
+		fmt.Printf(yellow("IP: %s\nCountry: %s\nRegion: %s\nCity: %s\nISP: %s\nOrg: %s\nLat/Lon: %.2f, %.2f\n"),
+			geo.Query, geo.Country, geo.Region, geo.City, geo.ISP, geo.Org, geo.Lat, geo.Lon)
+	},
+}
--- go.mod
+++ go.mod
@@ -4,14 +4,19 @@ go 1.23.4
 
 require (
 	github.com/Raqbit/mc-pinger v0.2.4 // direct
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // direct
+	github.com/fatih/color v1.18.0 // direct
+	github.com/gorcon/rcon v1.4.0 // direct
+	github.com/spf13/cobra v1.9.1 // direct
+)
+
+require (
+	github.com/Tnze/go-mc v1.20.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pires/go-proxyproto v0.6.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	golang.org/x/sys v0.25.0 // indirect
-	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // direct
-	github.com/fatih/color v1.18.0 // direct
-	github.com/spf13/cobra v1.9.1 // direct
-	github.com/gorcon/rcon v1.4.0 // direct
 )
