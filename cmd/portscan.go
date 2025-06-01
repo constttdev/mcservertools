3f7c17d3f4db73c2725337e0ef54174bfa210a14
constt <175602385+constttdev@users.noreply.github.com>
2025-06-01 14:13:31 +0200

    feat: changed pad size and added brackets

--- cmd/portscan.go
+++ cmd/portscan.go
@@ -30,31 +30,31 @@ var portscanCmd = &cobra.Command{
 		fmt.Printf(green("Scanning: %s\n\n"), portScanAddress)
 
 		var ports = map[int]string{
-			25565: "Minecraft Java (default)",
-			19132: "Minecraft Bedrock (UDP)",
-			19133: "Minecraft Bedrock IPv6 (UDP)",
-			8123:  "Dynmap (Web Plugin)",
-			25575: "RCON (Remote Console)",
-			25566: "BungeeCord/Waterfall Secondary",
-			24454: "GeyserMC (Bedrock Bridge)",
-			64738: "Mumble Voice Integration",
-			43594: "Custom Plugin API",
-			8080:  "Admin Panel / Dynmap HTTP",
-			8443:  "Admin Panel / Dynmap HTTPS",
-			3000:  "Custom Dashboard / Monitoring",
-			3306:  "MySQL (Plugin Database)",
-			5432:  "PostgreSQL (Plugin Database)",
+			25565: "(Minecraft Java (default))      ",
+			19132: "(Minecraft Bedrock (UDP))       ",
+			19133: "(Minecraft Bedrock IPv6 (UDP))  ",
+			8123:  "(Dynmap (Web Plugin))           ",
+			25575: "(RCON (Remote Console))         ",
+			25566: "(BungeeCord/Waterfall Secondary)",
+			24454: "(GeyserMC (Bedrock Bridge))     ",
+			64738: "(Mumble Voice Integration)      ",
+			43594: "(Custom Plugin API)             ",
+			8080:  "(Admin Panel / Dynmap HTTP)     ",
+			8443:  "(Admin Panel / Dynmap HTTPS)    ",
+			3000:  "(Custom Dashboard / Monitoring) ",
+			3306:  "(MySQL (Plugin Database))       ",
+			5432:  "(PostgreSQL (Plugin Database))  ",
 		}
 
 		for port, desc := range ports {
 			address := portScanAddress + ":" + strconv.Itoa(port)
 			conn, err := net.DialTimeout("tcp", address, time.Second)
 			if err != nil {
-				fmt.Printf(red("%-7s (%s) | Is closed or filterd\n"), strconv.Itoa(port), desc)
+				fmt.Printf(red("%-5s %s | Is closed or filterd\n"), strconv.Itoa(port), desc)
 				continue
 			}
 			conn.Close()
-			fmt.Printf(green("%-7s (%s) | Is open\n"), strconv.Itoa(port), desc)
+			fmt.Printf(green("%-5s %s | Is open\n"), strconv.Itoa(port), desc)
 		}
 	},
 }
